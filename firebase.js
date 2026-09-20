45ab5f2bf5bf5c8d9627895747c7c378b5edff5f
CoachFroden <frode@vibliz.com>
2026-09-20 11:50:31 +0200

    Add Cloud Functions client for guardian invitations

--- firebase.js
+++ firebase.js
@@ -1,5 +1,6 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-firestore.js";
+import { getFunctions } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-functions.js";
 const firebaseConfig={apiKey:"AIzaSyAKZMu2HZPmmoZ1fFT7DNA9Q6ystbKEPgE",authDomain:"samnanger-g14-f10a1.firebaseapp.com",projectId:"samnanger-g14-f10a1",storageBucket:"samnanger-g14-f10a1.firebasestorage.app",messagingSenderId:"926427862844",appId:"1:926427862844:web:5e6d11bb689c802d01b039",measurementId:"G-EJL3YYC63R"};
-export const app=initializeApp(firebaseConfig);export const auth=getAuth(app);export const db=getFirestore(app);
+export const app=initializeApp(firebaseConfig);export const auth=getAuth(app);export const db=getFirestore(app);export const functions=getFunctions(app,"europe-west1");
